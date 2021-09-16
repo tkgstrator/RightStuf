cdb8086b19a261187a6d391e1de62f224f77295f
tkgstrator <tkgstrator@users.noreply.github.com>
2021-09-16 11:03:30 +0900

    - インデックスページを更新

--- right.py
+++ right.py
@@ -22,29 +22,46 @@ class Product():
     publisher = None
     created = None
     runtime = None
+    promotion = None
+    orderType = None
 
-    def __init__(self, json):
-        self.title = json["Title"]
-        self.productId = json["ProductId"]
-        self.release = json["Release"]
-        self.price = json["Price"]
-        self.stock = json["In stock"]
-        self.spokenLanguages = [x.replace("'", "").replace("[", "").replace("]", "").strip() for x in json["Spoken Languages"].split(",")]
-        self.subtitleLanguages = [x.replace("'", "").replace("[", "").replace("]", "").strip() for x in json["SubTitle Languages"].split(",")]
-        self.productImageURL = json["ImageURL"]
-        self.publisher = json["Publisher"]
-        self.created = json["Created"]
-        self.runtime = json["Runtime"]
+    def __init__(self, item, promotion=None, orderType=None):
+        # 必須情報
+        self.title = item["displayname"]
+        self.productId = item["urlcomponent"]
+        self.release = dt.strptime(item["custitem_rs_release_date"], "%m/%d/%Y").strftime("%Y-%m-%d")
+        self.spokenLanguages = [x.replace("'", "").strip() for x in item["custitem_rs_spoken_language"].split(",")]
+        self.subtitleLanguages = [x.replace("'", "").strip() for x in item["custitem_rs_subtitle_language"].split(",")]
+        self.publisher = item["custitem_rs_publisher"]
+        self.price = item["onlinecustomerprice_detail"]["onlinecustomerprice"]
+        self.stock = item["isinstock"]
+        # プロモーションなどの区分
+        self.promotion = promotion
+        self.orderType = orderType
+        # ないかも知れない情報
+        primaryImageURLs = re.findall(r"https://.[^}]*primary.jpg", json.dumps(item["itemimages_detail"]))
+        if len(primaryImageURLs) > 0:
+            self.productImageURL = primaryImageURLs[0]
+        else:
+            pass
+        try:
+            self.created = item["custitem_rs_year_created"]
+        except KeyError:
+            pass
+        try:
+            self.runtime = item["custitem_rs_run_time"]
+        except KeyError:
+            pass
 
 
-def getProductsData(limit, offset):
+def getProductsData(limit, offset, promotion=None, orderType=None):
     url = "https://www.rightstufanime.com/api/items"
     params = {
         "c": "546372",
         "country": "US",
         "currency": "USD",
-        # "custitem_rs_spoken_language": "English",
-        # "custitem_rs_specials_and_promos" : "Weekly-Specials",
+        "custitem_rs_specials_and_promos": promotion,
+        "custitem_rs_new_releases_preorders": orderType,
         "custitem_rs_web_class": "Blu-ray",
         "language": "en",
         "fieldset": "details",
@@ -53,6 +70,50 @@ def getProductsData(limit, offset):
         "offset": offset
     }
     return(requests.get(url, params=params, verify=False))
+
+
+def getAllProductsData():
+    # プロモーションの種類で分ける
+    parameters = [
+        {
+            "promotion": None,
+            "orderType": None
+        },
+        {
+            "promotion": "Three-Day-Sale",
+            "orderType": None
+        },
+        {
+            "promotion": "Weekly-Specials",
+            "orderType": None
+        },
+        {
+            "promotion": "Daily-Deals",
+            "orderType": None
+        },
+        {
+            "promotion": None,
+            "orderType": "New-Release"
+        },
+        {
+            "promotion": None,
+            "orderType": "Pre-order"
+        }
+    ]
+    # プロモーションの種類でループ
+    items = []
+    for parameter in parameters:
+        promotion = parameter["promotion"]
+        orderType = parameter["orderType"]
+        # 総数を取得
+        res = getProductsData(0, 0, promotion, orderType)
+        if res.status_code == 200:
+            size = int(json.loads(res.text)["total"] / 100) + 1
+            for offset in range(0, size):
+                res = getProductsData(100, offset*100, promotion, orderType)
+                # 商品一覧を取得
+                items.extend(map(lambda x: Product(x, promotion, orderType), json.loads(res.text)["items"]))
+    return items
 
 
 def saveProductImage(iURL, saveDir):
@@ -66,49 +127,34 @@ def saveProductImage(iURL, saveDir):
             im.write(res.content)
 
 
-if __name__ == "__main__":
-    # CSVとして保存
-    with open("product.csv", mode="w", newline="") as f:
+def saveCSV(fileName):
+    headers = [
+        "Title", "ProductId", "Release", "Price", "In stock", "Publisher",
+        "Created", "Runtime", "Spoken Languages", "SubTitle Languages", "ImageURL",
+        "Promotion", "OrderType"
+    ]
+    with open(f"docs/public/csv/{fileName}.csv", mode="w") as f:
+        # CSVWriterの設定
         w = csv.writer(f)
-        w.writerow(["Title", "ProductId", "Release", "Price", "In stock", "Publisher",
-                   "Created", "Runtime", "Spoken Languages", "SubTitle Languages", "ImageURL"])
-        res = getProductsData(0, 0)
-        if res.status_code == 200:
-            size = int(json.loads(res.text)["total"] / 100) + 1
-            for offset in range(0, size):
-                res = getProductsData(100, offset*100)
-                items = json.loads(res.text)["items"]
-                for item in items:
-                    name = item["displayname"]
-                    productId = item["urlcomponent"]
-                    date = dt.strptime(item["custitem_rs_release_date"], "%m/%d/%Y").strftime("%Y-%m-%d")
-                    spokenLanguages = [x.replace("'", "").strip() for x in item["custitem_rs_spoken_language"].split(",")]
-                    subtitleLanguages = [x.replace("'", "").strip() for x in item["custitem_rs_subtitle_language"].split(",")]
-                    publisher = item["custitem_rs_publisher"]
-                    try:
-                        created = item["custitem_rs_year_created"]
-                    except KeyError:
-                        created = ""
-                    try:
-                        runtime = item["custitem_rs_run_time"]
-                    except KeyError:
-                        runtime = ""
-                    price = item["onlinecustomerprice_detail"]["onlinecustomerprice"]
-                    stock = item["isinstock"]
+        w.writerow(headers)
+        items: [Product] = getAllProductsData()
+        for item in items:
+            w.writerow([item.title, item.productId, item.release, item.price, item.stock, item.publisher, item.created, item.runtime,
+                       item.spokenLanguages, item.subtitleLanguages, item.productImageURL, item.promotion, item.orderType])
 
-                    primaryImageURLs = re.findall(r"https://.[^}]*primary.jpg", json.dumps(item["itemimages_detail"]))
-                    if len(primaryImageURLs) > 0:
-                        primaryImageURL = primaryImageURLs[0]
-                    else:
-                        primaryImageURL = ""
-                    w.writerow([name, productId, date, price, stock, publisher, created,
-                               runtime, spokenLanguages, subtitleLanguages, primaryImageURL])
 
+if __name__ == "__main__":
+
+    # 日付からCSVファイルの名前を決める
+    fileName = dt.strftime(dt.now(), "%Y%m%d")
+
+    # CSVファイルを保存
+    saveCSV(fileName)
     # CSVをJSONとして保存
-    with open("product.csv", mode="r") as f:
+    with open(f"docs/public/csv/{fileName}.csv", mode="r") as f:
         csvfile = []
         for row in csv.DictReader(f):
             csvfile.append(row)
         # JSON書き込み
-        with open(dt.strftime(dt.now(), "docs/public/json/%Y%m%d%H%M.json"), mode="w") as w:
+        with open(f"docs/public/json/{fileName}.json", mode="w") as w:
             json.dump(csvfile, w, indent=4, sort_keys=True)
